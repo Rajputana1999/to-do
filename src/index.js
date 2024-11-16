64120bd6bc56393772f8cca73a996b77191a5798
Rajputana1999 <jatinraj1999007@gmail.com>
2024-11-16 15:47:20 +0530

    restart from scratch

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const root = document.getElementById('root');
-const rootele = createRoot(root);
-
-rootele.render(<App/>);
